4e8a9845e940a31763e81881fb5b03d9b518081e
Valhalla-Wait <miha888g@yandex.ru>
2023-06-21 18:12:48 +0300

    fix import userSlice

--- src/core/store/slice/Users/index.ts
+++ src/core/store/slice/Users/index.ts
@@ -1,6 +1,6 @@
-import { authSlice } from './slice';
+import { usersSlice } from './slice';
 
 // export {signInSlice} from './slice'
-export const { actions, reducer } = authSlice;
+export const { actions, reducer } = usersSlice;
 export * as Selectors from './selectors';
 export * as Types from './types';
