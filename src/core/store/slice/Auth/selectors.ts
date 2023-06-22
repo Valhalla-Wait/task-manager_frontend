ea0f706cc747613832483d28d6b7282f08561a19
Valhalla-Wait <miha888g@yandex.ru>
2023-06-23 00:43:46 +0300

    fix2

--- src/core/store/slice/Auth/selectors.ts
+++
@@ -1,9 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit';
-import { RootStateType } from 'core/store/rootReducer';
-
-const selectAuth = (state: RootStateType) => state.signIn;
-
-export const AuthDataSelector = createSelector(
-  selectAuth,
-  (authState) => authState,
-);
