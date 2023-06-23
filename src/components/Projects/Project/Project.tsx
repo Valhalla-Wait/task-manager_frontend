3fb65ceacf6c8d10291293db7be4b274f9a510ea
Valhalla-Wait <miha888g@yandex.ru>
2023-06-23 10:17:31 +0300

    fix3

--- src/components/Projects/Project/Project.tsx
+++ src/components/Projects/Project/Project.tsx
@@ -14,6 +14,7 @@ import { TaskCard } from './TaskCard';
 
 import { Logout } from '../LogOut';
 import { LeftOutlined } from '@ant-design/icons';
+import { useState } from 'react';
 
 type MenuItemsType = 'tasks' | 'members' | 'analytics';
 
